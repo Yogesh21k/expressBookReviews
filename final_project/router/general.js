643de46a4035a167d82cbaa5991bb1bd89400989
Yogesh <yogesh.mc19@nsut.ac.in>
2024-07-29 13:03:24 +0530

    Get book details based on author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,8 +23,14 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let author=req.params.author;
+  const authorsBook=Object.values(books).filter((book)=>book.author===author)
+  if(authorsBook.length>0){
+      res.send(JSON.stringify(authorsBook,null,4))
+  }
+  else{
+      res.send("Unable to find the author");
+  }
 });
 
 // Get all books based on title
