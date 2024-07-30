bb68cd22de265ed8393a396f2b6f64a082effa44
Yogesh <yogesh.mc19@nsut.ac.in>
2024-07-30 10:18:40 +0530

    adding promise and async code

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -24,7 +24,8 @@ public_users.post("/register", (req,res) => {
   res.status(404).json({message:"Unable to register User!!!"});
 });
 
-
+/*
+// without promise/async 
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
@@ -77,6 +78,109 @@ public_users.get('/review/:isbn',function (req, res) {
         res.send("Unable to find the title");
     }
 });
+*/
+
+// using Promises
+
+// Get all books
+const listBooks = async () => {
+	try{
+		const getBooks = await Promise.resolve (books)
+		if (getBooks) {
+			return getBooks
+		} else {
+			return Promise.reject (new error('Books not found'))
+		}
+	} catch (error) {
+		console.log (error)
+	}
+}
+
+public_users.get('/',async (req, res)=> {
+  //Write your code here
+  const listBook = await listBooks()
+  res.json (listBook)
+});
+
+// Get books based on isbn
+const getByISBN=async(isbn)=>{
+  try{
+    const getISBN=await Promise.resolve(isbn);
+    if(getISBN){
+      return Promise.resolve(isbn)
+    }
+    else{
+      return Promise.reject(new error("Book with the isbn not found!"));
+    }
+  }
+  catch(error){
+    console.log(error);
+  }
+}
+
+public_users.get('/isbn/:isbn',async(req,res)=>{
+  const isbn=req.params.isbn;
+  const returnedIsbn=await getByISBN(isbn);
+  res.send(books[returnedIsbn]);
+})
+
+  // Get books based on author
+const getByAuthor=async(author)=>{
+  try{
+    
+    if(author){
+      const authBook=[];
+      Object.values(books).map((book)=>{
+      if(book.author===author){
+        authBook.push(book);
+      }})
+      return Promise.resolve(authBook);
+    }
+    else{
+      return Promise.reject(new error("Book with the author name not found!!!"));
+    }
+    
+  }
+  catch(error){
+    console.log(error);
+  }
+}
+
+
+  public_users.get('/author/:author',async(req,res)=>{
+    const author=req.params.author;
+    const data=await getByAuthor(author);
+    res.send(data);
+  })
+
+  // Get books based on title
+const getByTitle=async(title)=>{
+  try{
+    
+    if(title){
+      const titleBook=[];
+      Object.values(books).map((book)=>{
+      if(book.title===title){
+        titleBook.push(book);
+      }})
+      return Promise.resolve(titleBook);
+    }
+    else{
+      return Promise.reject(new error("Book with the author name not found!!!"));
+    }
+    
+  }
+  catch(error){
+    console.log(error);
+  }
+}
+
+
+  public_users.get('/title/:title',async(req,res)=>{
+    const title=req.params.title;
+    const data=await getByAuthor(title);
+    res.send(data);
+  })
 
 
 module.exports.general = public_users;
