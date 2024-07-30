4300a7596a35862077052b02d2325d1284b60383
Yogesh <yogesh.mc19@nsut.ac.in>
2024-07-30 10:16:08 +0530

    Add and Delete a book review

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -59,49 +59,33 @@ regd_users.post("/login", (req,res) => {
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  const username=req.body.username;
-  const password=req.body.password;
-
-  if(!username || !password){
-    return res.status(404).json({message:"User Not Found!!!"});
-  }
-
-  if(authenticatedUser(username,password)){
-    let isbn=req.params.isbn;
-    let book=books[isbn];
-    if(book){
-      let review=req.body.reviews;
-      if(review){
-        book["reviews"]=review;
-      }
-      books[isbn]=book;
-      res.send(`Books with isbn ${isbn} updated its review`);
+    const isbn = req.params.isbn;
+    const review = req.body.review;
+    const username = req.session.authorization.username;
+    console.log("add review: ", req.params, req.body, req.session);
+    if (books[isbn]) {
+        let book = books[isbn];
+        book.reviews[username] = review;
+        return res.status(200).send("Review successfully posted");
     }
-    else{
-      res.status(404).json({message:`Books with the isbn ${isbn} Not found!!!`});
+    else {
+        return res.status(404).json({message: `ISBN ${isbn} not found`});
     }
-  }
-
- });
+});
 
 // Delete a book review
-regd_users.delete("/auth/review/:isbn",(req,res)=>{
-  const username=req.body.username;
-  const password=req.body.password;
-
-  if(!username || !password){
-    return res.status(404).json({message:"User Not Found!!!"});
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  const username = req.session.authorization.username;
+  if (books[isbn]) {
+      let book = books[isbn];
+      delete book.reviews[username];
+      return res.status(200).send("Review successfully deleted");
   }
-
-  if(authenticatedUser(username,password)){
-    let isbn=req.params.isbn;
-    
-    if(isbn){
-      delete books["reviews"];
-    }
-    res.send(`Book with the isbn ${isbn} has been deleted.`);
+  else {
+      return res.status(404).json({message: `ISBN ${isbn} not found`});
   }
-})
+});
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,6 +25,7 @@ public_users.post("/register", (req,res) => {
 });
 
 
+
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   res.send(JSON.stringify(books,null,4));
@@ -76,5 +77,6 @@ public_users.get('/review/:isbn',function (req, res) {
         res.send("Unable to find the title");
     }
 });
+
 
 module.exports.general = public_users;
